a52e12a726323fec4f2cd3b2fd8ed19e96a9c572
MathieuAuclair <mathiass12@hotmail.com>
2017-09-03 14:33:30 -0400

    improving guessing game tutorial

--- guessing_game_tutorial/src/main.rs
+++ guessing_game_tutorial/src/main.rs
@@ -2,12 +2,26 @@ extern crate rand;
 
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
+    let mut win = false;
     println!("Guess a number");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess)
         .expect("failed to read line");
     let answer = rand::thread_rng().gen_range(1,11);
-    println!("You guessed: {} and the number was {}", guess, answer);
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+    match guess.cmp(&answer){
+        Ordering::Less => println!("too low"),
+        Ordering::Greater => println!("too high"),
+        Ordering::Equal => win = true,
+    }
+
+    if !win {
+        main();
+    } else{
+        println!("you guessed the number! you won!");
+    }
 }
