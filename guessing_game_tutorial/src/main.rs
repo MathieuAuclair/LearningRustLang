15eea568479a0fa39737a0cce8ef8d0f4841f3c2
MathieuAuclair <mathiass12@hotmail.com>
2017-09-03 01:00:00 -0400

    learning random number

--- guessing_game_tutorial/src/main.rs
+++ guessing_game_tutorial/src/main.rs
@@ -1,10 +1,13 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess a number");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess)
         .expect("failed to read line");
-
-    println!("You guessed: {}", guess);
+    let answer = rand::thread_rng().gen_range(1,11);
+    println!("You guessed: {} and the number was {}", guess, answer);
 }
